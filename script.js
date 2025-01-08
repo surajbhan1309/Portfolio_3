463ab287d6b96f8ae4098b78e72e4b3ad64df7a1
surajbhan1309 <151545643+surajbhan1309@users.noreply.github.com>
2025-01-08 23:57:42 +0530

    Add files via upload
    done

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const title = document.querySelector('.section-tittle');
+//     setInterval(() => {
+//         title.style.animation = 'none';
+//         title.offsetHeight; // Trigger reflow to restart the animation
+//         title.style.animation = 'typing 3.5s steps(40, end) infinite, blink-caret 0.75s step-end infinite';
+//     }, 4000); // Restart the typing effect every 4 seconds
+// });
